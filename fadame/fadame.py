d248b13573f019a7f1ceaa809b015d9aaa8c0fec
Krittin Phornsiricharoenphant <oatkrittin@gmail.com>
2018-11-30 22:43:05 +0700

    implement with out test cases, just manual test.

--- fadame/fadame.py
+++ fadame/fadame.py
@@ -14,6 +14,15 @@ MODULE_TO_MERGE = [
   # 'Sequence Duplication Levels',
   # 'Adapter Content'
 ]
+BASIC_STATISTICS_MEASURE_ORDERS = { 
+  'Filename':0, 
+  'File type': 1, 
+  'Encoding': 2,
+  'Total Sequences': 3,
+  'Sequences flagged as poor quality': 4,
+  'Sequence length': 5,
+  '%GC':6
+}
 
 class FadapaObj(Fadapa):
   def __eq__(self, other):
@@ -29,14 +38,57 @@ def load_one(path):
 def load_many(paths):
   return map(load_one, paths)
 
+BASIC_STATISTIC_FIELD_TO_MERGE = ['Sequences flagged as poor quality', 'Total Sequences']
+def addSortable(module):
+  def func(obj):
+    data = obj.clean_data(module)
+    headers = data[0]
+    if module == 'Basic Statistics':
+      orders = BASIC_STATISTICS_MEASURE_ORDERS
+      objectValue = { value[0]: (int(value[1]) if value[0] in BASIC_STATISTIC_FIELD_TO_MERGE else value[1]) for value in data[1:] }
+    else:
+      orders = { value[0]: int(value[0].split('-')[0]) for value in data[1:] }
+      objectValue = { value[0]: float(value[1]) for value in data[1:] }
+    return headers, orders, objectValue
+  return func
+
+def mergeByModule(module):
+  def reduced(acc, value):
+    (accHeaders, accOrders, accObjectValue) = acc
+    (headers, orders, objectValue) = value
+    for k, v in objectValue.iteritems():
+        if k in accObjectValue:
+          if module == 'Basic Statistics':
+            if k in BASIC_STATISTIC_FIELD_TO_MERGE:
+              accObjectValue[k] += v
+          else:
+            accObjectValue[k] += v
+        else:
+          accObjectValue[k] = v
+    accOrders.update(orders)
+    return (headers, accOrders, accObjectValue)
+  return reduced
+
 def merge(fadapas):
-  return FadapaObj('tests/merged.fastqc.data.txt')
+  fadapaObjs = load_many(fadapas)
+  output = "##FastQC	0.11.8\n"
+  summaries = map(lambda obj: { v[1]: (v[0] == "pass") for v in obj.summary()[1:] }, fadapaObjs)
+  summary = reduce(lambda acc, obj: { k: acc[k] and v for k, v in obj.iteritems() }, summaries)
+  for module in MODULE_TO_MERGE:
+    output += ">>%s\t%s\n" % (module, "pass" if summary[module] else "warn")
+    parsed = map(addSortable(module), fadapaObjs)
+    (headers, orders, objectValue) = reduce(mergeByModule(module), parsed[1:], parsed[0])
+    output += "#%s\n" % ("\t".join(headers))
+    for key, value in sorted(orders.iteritems(), key=lambda (k,v): (v,k)):
+      output += "%s\t%s\n" % (key, objectValue[key])
+    output += ">>END_MODULE\n"
+  # print(fadapaObjs[0].clean_data('Basic Statistics'))
+  # print(fadapaObjs[0].summary())
+  return output
 
 def fadame_merge(args):
-  print('test')
-  return True
-  # print(args.input)
-  # print(args.output)
+  with open(args.output, 'w') as f:
+    f.write(merge(args.input))
  
 def main():
   parser = argparse.ArgumentParser(description='Process some integers.')
--- tests/test_fadame.py
+++ tests/test_fadame.py
@@ -3,14 +3,29 @@ from fadame import FadapaObj, load_one, load_many, merge, fadame_merge
 def inc(x):
     return x + 1
 
+all_reads = [
+  "tests/sampleId_1_1.fastqc.data.txt",
+  "tests/sampleId_1_2.fastqc.data.txt",
+  "tests/sampleId_2_1.fastqc.data.txt",
+  "tests/sampleId_2_2.fastqc.data.txt",
+  "tests/sampleId_3_1.fastqc.data.txt",
+  "tests/sampleId_3_2.fastqc.data.txt",
+  "tests/sampleId_4_1.fastqc.data.txt",
+  "tests/sampleId_4_2.fastqc.data.txt",
+  "tests/sampleId_5_1.fastqc.data.txt",
+  "tests/sampleId_5_2.fastqc.data.txt",
+  "tests/sampleId_6_1.fastqc.data.txt",
+  "tests/sampleId_6_2.fastqc.data.txt",
+  "tests/sampleId_7_1.fastqc.data.txt",
+  "tests/sampleId_7_2.fastqc.data.txt",
+  "tests/sampleId_8_1.fastqc.data.txt",
+  "tests/sampleId_8_2.fastqc.data.txt"
+]
 
 def test_answer():
     assert inc(3) == 4
 
-def test_fadame_merge():
-    assert fadame_merge([])
-
 def test_merge():
     expected = load_one('tests/merged.fastqc.data.txt')
-    merged = merge([])
-    assert expected == merged
+    merged = merge(all_reads)
+    assert False
